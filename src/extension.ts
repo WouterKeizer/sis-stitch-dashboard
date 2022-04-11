645dbd6970399420a3a717926cdf80c4d9724108
Wouter Keizer <wouterkeizer@shipitsmarter.com>
2022-04-11 22:07:12 +0200

    feat: carrier selection

--- src/extension.ts
+++ src/extension.ts
@@ -16,112 +16,55 @@ export function activate(context: vscode.ExtensionContext) {
 		  }
 		);
   
-		// And set its HTML content
-			
 		const updateWebview = (carrier: string) => {
 			getMyWebviewContent(panel.webview, context, carrier).then(html =>panel.webview.html = html);   // <--- HTML
 		  };
 
+		//Creates a webview with as default carrier selection
 		updateWebview("tnt");
-
-
-		// Get path to script on disk
-			let scriptPath = vscode.Uri.file(
-				path.join(context.extensionPath, 'scripts', 'script.ps1')
-			);
-			// let scriptPath = Uri.URI.file(context.asAbsolutePath(path.join('scripts', 'script.ps1')));
-
-			// Pre-allocate terminal and terminalExists
-			let terminal: vscode.Terminal;
-			let terminalExists: boolean;
 
 		panel.webview.onDidReceiveMessage(
 			message => {
-				// check if terminal exists and is still alive
-				terminalExists = (terminal && !(terminal.exitStatus));
-
 				switch (message.command) {
-					case 'startScript':
-						if (!terminalExists) {
-							vscode.window.showErrorMessage(message.text);
-							terminal = startScript('','',`Write-Host 'Hello World!'`);
-						} else {
-							vscode.window.showErrorMessage('Script already started!');
-						}
-						break;
-
 					case 'startCarrier':
-						
-						//updateWebview(message.text);
-						terminal = startScript('','',message.text);
-
+						updateWebview(message.text);
 						break;
-				
 				}
-
 				return;
 			},
 			undefined,
 			context.subscriptions
 		);
 	  })
-	  	
 	);
-}
-
-function startScript (fileName ?: string , filePath ?: string , command ?: string) : vscode.Terminal {
-	let terminal = vscode.window.createTerminal('bram');
-	terminal.show();
-	//terminal.sendText('Get-Location');
-	if (filePath && filePath !== '') {
-		terminal.sendText(`cd ${filePath}`);
-	};
-	
-	if (fileName && fileName !== '') {
-		terminal.sendText(`./${fileName}`);
-	};
-
-	if (command && command !== '') {
-		terminal.sendText(command);
-	};
-	
-	return terminal;
 }
 
   async function getMyWebviewContent(webview: vscode.Webview, context: any, carrier:string) : Promise<string> { 
 	let html: string = ``;
-	let foldersHtml: string = ``;
 	
-	const myStyle = webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, 'media', 'style.css'));   // <--- 'media' is the folder where the .css file is stored
-	const scriptURI = webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, 'src/scripts', 'main.js'));   // <--- 'media' is the folder where the .css file is stored
+	const styleURI = webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, 'media', 'style.css'));   
+	const scriptURI = webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, 'src/scripts', 'main.js'));  
 	
 	let carrierlist = await rCarrierList(carrier);
-	// construct your HTML code
+
 	html += `
 			<!DOCTYPE html>
 			<html>
 				<head>
-				  	<link href="${myStyle}" rel="stylesheet" />  
+				  	<link href="${styleURI}" rel="stylesheet" />  
 					<script src="${scriptURI}"></script> 
 				</head>
 				<body>
-				<div>
-					<button class="button-34" role="button" onclick="startScript()" id="startScript">Start script</button>
-					<button class="button-34" role="button" onclick="startCarrier()" id="startCarrier">Start Carrier</button>
-				</div>
-				  <div class="main"> 
-					<label for="carriers">Choose a carrier:</label>
-					<select id="terst">
+				<div class="header">
+					<button role="button" onclick="selectCarrier()" id="startCarrier">Start Carrier</button>
+					<select id="SelectedCarrier">
 						${getCarrierOptionList()}
 					</select>
-
-					<input type="text" maxlength="512" id="ScriptCommand" class="searchField"/>
-
-					<button class="button-34" role="button" onclick="renderCarrierList()" id="renderCarrierList">Refresh</button>
-
-
-
-				  </div>
+				</div>
+				<div class="main">
+				  <h1>${carrier}</h1>
+					${carrierlist}
+				<div>
 				</body>
 			 </html>
 	`;
@@ -129,32 +72,29 @@ function startScript (fileName ?: string , filePath ?: string , command ?: strin
 	return html;
   }
 
+function getCarriers(){
+	let carriers= ["ups", "tnt", "dhl", "dpd", "dsv", "rhenus"];
+	return carriers;
+}
 
-  function getCarriers(){
-	  let carriers= ["ups", "tnt", "dhl", "dpd", "dsv", "rhenus"];
-	  return carriers;
-  }
-
-  function getCarrierOptionList(): string {
+function getCarrierOptionList(): string {
 	let html: string = ``;
-	
 	getCarriers().forEach(carrier => {
 		html+= `<option value="${carrier}">${carrier}</option>`;
 	});
 	return html;
-  }
+}
 
-  async function rCarrierList(selectedCarrier:string): Promise<string> {
+async function rCarrierList(srCarrier:string): Promise<string> {
 	let html: string = ``;
-	let allIntegrations = await vscode.workspace.findFiles(`**/carriers/${selectedCarrier}/**`);
+	let allIntegrations = await vscode.workspace.findFiles(`**/carriers/${srCarrier}/**`);
 
 	allIntegrations.forEach(async element => {
 		html += `<li>${element.path}</li>`;
 
-		// Example on reading file
-		// let document = await vscode.workspace.openTextDocument(element.path);
-		// document.getText();
+	// Example on reading file
+	// let document = await vscode.workspace.openTextDocument(element.path);
+	// document.getText();
 	});
-
 	return html;
-  }
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,6 @@
 const vscodeApi = acquireVsCodeApi(); 
 
-var ScriptArgumentsField = document.getElementById('ScriptArguments');
-ScriptArgumentsField.addEventListener("keydown", function (e) {
-    if (e.key === "Enter") {  
-        executeScript();
-    }
-});
-var ScriptCommandField =  document.getElementById('ScriptCommand');
-
-function startScript(){
-    vscodeApi.postMessage({command: "startScript", text: "Start Selected Script"});
-  };
-
-  function startCarrier(){
-    vscodeApi.postMessage({command: "startCarrier", text: `${ScriptCommandField.value}`});
-  };
+const selectCarrier = () =>{
+  let sCarrier = document.querySelector("#SelectedCarrier").value; 
+  vscodeApi.postMessage({command: "startCarrier", text: sCarrier});
+};
